7fc56a5f0ee6d427d5c097feb9b6bd830186780c
KhanhND <khanhnd162@gmail.com>
2022-04-22 10:30:17 +0700

    implement update session

--- manager/session/update.go
+++ manager/session/update.go
@@ -1,8 +1,11 @@
 package session
 
 import (
+	"github.com/scylladb/gocqlx/v2/qb"
 	scyna "github.com/scyna/go"
 	"google.golang.org/protobuf/proto"
+	"log"
+	"time"
 )
 
 func Update(data []byte) {
@@ -12,6 +15,15 @@ func Update(data []byte) {
 		return
 	}
 
-	/*TODO*/
-
+	if applied, err := qb.Update("scyna.session").
+		Set("last_update").
+		Where(qb.Eq("id")).
+		Existing().
+		Query(scyna.DB).
+		Bind(time.Now(), signal.ID).
+		ExecCASRelease(); !applied {
+		if err != nil {
+			log.Print("Can not update UpdateSessionSignal: ", err.Error())
+		}
+	}
 }
